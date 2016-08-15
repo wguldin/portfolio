924c13ac8bc609978428538dac0ee77309b79224
Will <will@britecore.com>
2016-08-15 08:12:41 -0500

    star modifications

--- js/main.js
+++ js/main.js
@@ -26,15 +26,20 @@ var headerGraphic = (function() {
         clock[0].innerHTML = displayTime.toString();
     };
 
-    self.starCount = 120;
+    self.starCount = 160;
 
     self.createGraphic = function() {
         var timePeriod = self.setTimePeriod();
+        var body = document.getElementsByTagName('BODY')[0];
 
         if (timePeriod === 'day') {
+            self.addClass(body, 'theme--day');
+
             // self.createClouds();
         }
         else if (timePeriod === 'night') {
+            self.addClass(body, 'theme--night');
+
             for (var i = 0; i < self.starCount; i++) {
                 var star = self.createStar();
 
@@ -47,7 +52,7 @@ var headerGraphic = (function() {
         var hours = self.today.getHours();
 
         if (hours >= 5 && hours <= 19) {
-            return 'day';
+            return 'night';
         } else {
             return 'night';
         }
@@ -61,12 +66,12 @@ var headerGraphic = (function() {
         var y = self.randomIntFromInterval(0, self.svgHeight);
         var deg = self.randomIntFromInterval(0, 360);
 
-        var weighted = Math.pow(Math.random(), .3);
-        var scaled = Math.floor(weighted * (max - min + 1)) + min;
+        var weightedScale = Math.pow(Math.random(), .3);
+        var weightedOpacity = Math.pow(Math.random(), .3) - .1;
 
         self.setAttributes(star, {
-            'transform': 'translate(' + x + ' ' + y + ') rotate(' + deg + ')',
-            'style': 'opacity: .6; fill: #fff;'
+            'transform': 'translate({0} {1}) rotate({2}) scale({3})'.format(x, y, deg, weightedScale),
+            'style': 'opacity: {0}; fill: #fff;'.format(weightedOpacity)
         });
 
         return star;
@@ -82,6 +87,54 @@ var headerGraphic = (function() {
 
     self.randomIntFromInterval = function(min, max) {
         return Math.floor(Math.random()*(max-min+1)+min);
+    };
+
+    // Taken from Jake Trent (http://jaketrent.com/post/addremove-classes-raw-javascript/)
+    self.hasClass = function(el, className) {
+        if (el.classList) {
+            return el.classList.contains(className);
+        }
+        else {
+            return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+        }
+    };
+
+    self.addClass = function(el, className) {
+        if (el.classList) {
+            el.classList.add(className);
+        }
+        else if (!self.hasClass(el, className)) {
+            el.className += ' ' + className;
+        }
+    };
+
+    self.removeClass = function(el, className) {
+        if (el.classList) {
+            el.classList.remove(className);
+        }
+        else if (self.hasClass(el, className)) {
+            var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+            el.className = el.className.replace(reg, ' ');
+        }
+    };
+
+    /*
+    Function: format
+    Give all Strings a format method. The kind where format strings are like:
+        "Here is the first arg: {0}, and here is the second: {1}."
+        Where {0} and {1} will be replaced by the first and second arguments
+        that are passed to the format function.
+    */
+    String.prototype.format = function() {
+        var string = this;
+
+        for (var i = 0; i < arguments.length; i++) {
+            var text = arguments[i];
+
+            string = string.replace(new RegExp('\\{'+i+'\\}', 'gm'), text);
+        }
+
+        return string;
     };
 
     return self;
