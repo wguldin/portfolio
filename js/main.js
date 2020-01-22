7d0d9b9b21825a6f0f82f6a9aad83c90166311a8
Will <will@britecore.com>
2020-01-22 11:46:04 -0600

    Redesign

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
 // Declutter global namespace
 var folio = {
     ajax: {},
-    graphic: {},
     utils: {}
 };
 
@@ -128,118 +127,6 @@ folio.ajax = (function() {
     return self;
 })();
 
-folio.graphic = (function() {
-    var self = {};
-
-    self.svg = document.getElementById('illustration');
-
-    self.init = function() {
-        if(self.svg) {
-            var svgDimensions = self.svg.getBoundingClientRect();
-
-            self.svgWidth  = svgDimensions.width;
-            self.svgHeight = svgDimensions.height;
-
-            self.createGraphic();
-        }
-    };
-
-    self.lineCount = 48;
-
-    self.createGraphic = function() {
-        var graphic = SVG('#illustration');
-
-        for (var i = 0; i < self.lineCount; i++) {
-            self.createLine(graphic);
-        }
-    };
-
-    self.createLine = function(graphic) {
-        var coords = self.setLineCoordinates();
-
-        // Favor higher opacity values, so most lines show up.
-        var weightedOpacity = Math.pow(Math.random(), .25) - .1;
-
-        // Set animation speed relative to distance.
-        var distance = self.getDistance(coords);
-        var speed = distance/2;
-
-        var line = graphic.line(coords["x1"], coords["y1"], coords["x1"], coords["y1"]);
-        
-        line.animate(500, 250, 'now').plot([[coords["x1"], coords["y1"]], [coords["x2"], coords["y2"]]]);
-
-        line.attr({'style': 'opacity: {0}'.format(weightedOpacity)});
-    };
-
-    self.setLineCoordinates = function() {
-        var startSide = self.selectSide();
-        var startPoint = self.selectPoint(startSide);
-
-        var finalSide = self.selectSide(startSide);
-        var finalPoint = self.selectPoint(finalSide);
-
-        return {
-            'x1': startPoint['x'],
-            'y1': startPoint['y'],
-            'x2': finalPoint['x'],
-            'y2': finalPoint['y']
-        };
-    };
-
-    self.getDistance = function(coords) {
-        var a = coords['x1'] - coords['x2'];
-        var b = coords['y1'] - coords['y2'];
-
-        var c = Math.sqrt( a*a + b*b );
-
-        return c;
-    };
-
-    self.selectPoint = function(side) {
-        var point = {'x': null, 'y': null};
-
-        switch(side) {
-            case 't':
-                point['x'] = folio.utils.randomIntFromInterval(0, self.svgWidth);
-                point['y'] = self.svgHeight;
-                break;
-
-            case 'b':
-                point['x'] = folio.utils.randomIntFromInterval(0, self.svgWidth);
-                point['y'] = 0;
-                break;
-
-            case 'l':
-                point['x'] = 0;
-                point['y'] = folio.utils.randomIntFromInterval(0, self.svgHeight);
-                break;
-
-            case 'r':
-                point['x'] = self.svgWidth;
-                point['y'] = folio.utils.randomIntFromInterval(0, self.svgHeight);
-                break;
-        }
-
-        return point;
-    };
-
-    self.selectSide = function (existingSide) {
-        var sides = ['t', 'b', 'l', 'r'];
-
-        if(existingSide){
-            var i = sides.indexOf(existingSide);
-
-            if(i != -1) {
-                sides.splice(i, 1);
-            }
-        }
-
-        return sides[Math.floor(Math.random()*sides.length)];
-    };
-
-    return self;
-})();
-
 folio.utils.randomIntFromInterval = function(min, max) {
     return Math.floor(Math.random()*(max-min+1)+min);
 };
@@ -311,6 +198,5 @@ String.prototype.format = function() {
 };
 
 (function() {
-    folio.graphic.init();
     folio.ajax.init();
 })();
